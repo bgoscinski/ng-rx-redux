ec5237a1cfea209b70b5312819511dd2a0807fae
Bartosz Gościński <bargosc@gmail.com>
2016-03-22 10:54:03 +0100

    cleanup package.json

--- jspm.config.js
+++ jspm.config.js
@@ -21,17 +21,31 @@ SystemJS.config({
   ],
   map: {
     "angular": "github:angular/bower-angular@1.5.2",
+    "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
     "core-js": "npm:core-js@1.2.6",
     "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
+    "lodash": "npm:lodash@4.6.1",
     "path": "github:jspm/nodelibs-path@0.2.0-alpha",
     "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.2",
     "process": "github:jspm/nodelibs-process@0.2.0-alpha",
     "rxjs": "npm:rxjs@5.0.0-beta.2"
   },
   packages: {
+    "github:jspm/nodelibs-buffer@0.2.0-alpha": {
+      "map": {
+        "buffer-browserify": "npm:buffer@4.5.0"
+      }
+    },
     "github:frankwallis/plugin-typescript@4.0.2": {
       "map": {
         "typescript": "npm:typescript@1.8.9"
+      }
+    },
+    "npm:buffer@4.5.0": {
+      "map": {
+        "base64-js": "npm:base64-js@1.1.1",
+        "ieee754": "npm:ieee754@1.1.6",
+        "isarray": "npm:isarray@1.0.0"
       }
     }
   }
